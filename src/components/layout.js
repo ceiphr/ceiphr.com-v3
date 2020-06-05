b366d3d01231853159fcfb041d3f42378b4c640b
Ari Birnbaum <ari@ceiphr.com>
2020-06-05 18:14:04 -0400

    Refactored sass

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link } from "gatsby"
 
-import "../styles/app.scss"
+import "../styles/index.scss"
 
 const Layout = ({ location, title, children }) => {
   const rootPath = `${__PATH_PREFIX__}/`
