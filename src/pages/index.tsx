3caa8bab54a12c635f545bec19bf58322a3f4468
Ari Birnbaum <ari@ceiphr.com>
2021-01-05 04:30:04 -0500

    Reverted. Causes flicker

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -76,7 +76,7 @@ const BlogIndex = ({ data, location }: PageProps<Data>) => {
       <SEO title="No-nonsense technical articles from Ari Birnbaum" />
       {schema}
 
-      <section className="hero hero-homepage is-halfheight is-primary">
+      <section className="hero hero-homepage is-halfheight is-transparent">
         <div className="hero-body">
           <div className="container">
             <div className="hero-banner">
